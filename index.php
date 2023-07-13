525bf988b1444ea570999a031669d487b12def67
Livni <llmc0383@gmail.com>
2023-07-13 15:37:59 -0600

    configuracion de tabla

--- index.php
+++ index.php
@@ -7,22 +7,40 @@
 </head>
 <body>
 <h1>AJAX</h1>
-<h2>CONSULTA DE POKEMON</h2>
-<form action="">
+    <h2>CONSULTA A POKEMON API</h2>
+    <form>
         <input type="text" name="pokemon" id="pokemon">
-        <button type="submit">Consultar</button>
-</form>
-<br<>
-<table>
-    <thead>
-        <tbody>
+        <button type="submit">Consultar</button> 
+        <button id="buttonConsulta" type="button">Consultar a PHP</button>
+    </form>
+    <br>
+    <p id="estado"></p>
+    <table border="1px solid">
+        <thead>
             <tr>
                 <th>DATO</th>
                 <th>VALOR</th>
+            </tr>
+        </thead>
+        <tbody>
             <tr>
-            <thead>
-</table>
-
+                <td>NOMBRE</td>
+                <td id="nombrePokemon"></td>
+            </tr>
+            <tr>
+                <td>PESO</td>
+                <td id="pesoPokemon"></td>
+            </tr>
+            <tr>
+                <td>TIPO</td>
+                <td id="tipoPokemon"></td>
+            </tr>
+            <tr>
+                <td>IMAGEN</td>
+                <td><img src="" alt="" id="imagenPokemon"></td>
+            </tr>
+        </tbody>
+    </table>
   <script src="script.js"></script>
 </body>
 </html>
