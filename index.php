4d31506fa71b80f7416ba346bf2a42bbed907afd
Livni <llmc0383@gmail.com>
2023-07-13 15:14:20 -0600

    index

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ajax</title>
+</head>
+<body>
+<h1>AJAX</h1>
+<h2>CONSULTA DE POKEMON</h2>
+<form action="">
+        <input type="text" name="pokemon" id="pokemon">
+        <button type="submit">Consultar</button>
+</form>
+<br<>
+<table>
+    <thead>
+        <tbody>
+            <tr>
+                <th>DATO</th>
+                <th>VALOR</th>
+            <tr>
+            <thead>
+</table>
+
+  <script src="script.js"></script>
+</body>
+</html>
